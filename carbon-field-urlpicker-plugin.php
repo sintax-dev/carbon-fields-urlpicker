546f25b811b22b5618d6a61cd31d547e33c05d2d
Jacson Junior <jacson@sintax.dev>
2021-07-17 20:25:57 -0300

    Update carbon-field-urlpicker-plugin.php

--- carbon-field-urlpicker-plugin.php
+++ carbon-field-urlpicker-plugin.php
@@ -15,3 +15,14 @@
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require( __DIR__ . '/vendor/autoload.php' );
 }
+
+use Carbon_Fields\Carbon_Fields;
+use Carbon_Field_UrlPicker\UrlPicker_Field;
+
+define('Carbon_Field_UrlPicker\\DIR', __DIR__);
+
+add_action('after_setup_theme', function () {
+	Carbon_Fields::extend(UrlPicker_Field::class, function ($container) {
+		return new UrlPicker_Field( $container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label'] );
+	});
+}, 99);
